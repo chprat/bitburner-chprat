f6e581e5202a81586c75f3ce82b54916fa012619
chprat <cprater@gmx.net>
2022-04-03 10:29:56 +0200

    Use getServer in hacker.js and make it less chatty

--- hacker.js
+++ hacker.js
@@ -1,35 +1,44 @@
 import { listServers } from 'imports/scanner.js'
 
-export async function hack (ns, server) {
-  if (!ns.hasRootAccess(server)) {
+export async function hack (ns, serverName) {
+  const server = ns.getServer(serverName)
+  if (!server.hasAdminRights) {
     return
   }
-  if (ns.getServerMaxMoney === 0) {
+  if (server.moneyMax === 0) {
     return
   }
-  const hackingLevel = ns.getHackingLevel()
-  const serverHackingLevel = ns.getServerRequiredHackingLevel(server)
-  if (hackingLevel < serverHackingLevel) {
+  if (ns.getHackingLevel() < server.requiredHackingSkill) {
     return
   }
-  const moneyThresh = Math.floor(ns.getServerMaxMoney(server) * 0.75)
-  const securityThresh = Math.floor(ns.getServerMinSecurityLevel(server) * 1.25)
-  const currentMoney = Math.floor(ns.getServerMoneyAvailable(server))
-  const serverSecurityLevel = Math.floor(ns.getServerSecurityLevel(server))
-  if (serverSecurityLevel > securityThresh) {
-    ns.print('SecurityLevel: ' + serverSecurityLevel + ' > ' + securityThresh)
-    await ns.weaken(server)
-  } else if (currentMoney < moneyThresh) {
-    ns.print(`Money: ${ns.nFormat(currentMoney, '$0.000a')} < ${ns.nFormat(moneyThresh, '$0.000a')}`)
-    await ns.grow(server)
+  const moneyThresh = Math.floor(server.moneyMax * 0.75)
+  const securityThresh = Math.floor(server.minDifficulty * 1.25)
+  if (server.hackDifficulty > securityThresh) {
+    await ns.weaken(serverName)
+    const newServerSecurityLevel = ns.getServerSecurityLevel(serverName)
+    ns.print(`${serverName} weakened by ${Math.floor(server.hackDifficulty - newServerSecurityLevel)} to ${Math.floor(newServerSecurityLevel)} in ${ns.tFormat(ns.getWeakenTime(serverName))}`)
+  } else if (server.moneyAvailable < moneyThresh) {
+    await ns.grow(serverName)
+    const newCurrentMoney = ns.getServerMoneyAvailable(serverName)
+    ns.print(`${serverName} growed by ${ns.nFormat(newCurrentMoney - server.moneyAvailable, '$0.000a')} to ${ns.nFormat(newCurrentMoney, '$0.000a')} in ${ns.tFormat(ns.getGrowTime(serverName))}`)
   } else {
-    await ns.hack(server)
+    const hacked = await ns.hack(serverName)
+    ns.print(`${serverName} hacked by ${ns.nFormat(hacked, '$0.000a')} to ${ns.nFormat(ns.getServerMoneyAvailable(serverName), '$0.000a')} in ${ns.tFormat(ns.getHackTime(serverName))}`)
   }
 }
 
 /** @param {NS} ns **/
 export async function main (ns) {
   ns.disableLog('sleep')
+  ns.disableLog('getHackingLevel')
+  ns.disableLog('getServerRequiredHackingLevel')
+  ns.disableLog('weaken')
+  ns.disableLog('hack')
+  ns.disableLog('grow')
+  ns.disableLog('getServerMinSecurityLevel')
+  ns.disableLog('getServerSecurityLevel')
+  ns.disableLog('getServerMaxMoney')
+  ns.disableLog('getServerMoneyAvailable')
   const servers = listServers(ns).filter(s => s !== 'darkweb')
     .filter(s => s !== 'home')
     .filter(s => s !== ns.getHostname())

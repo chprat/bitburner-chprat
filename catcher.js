9e234545f33bb374bc35de9fc0c87eb8d7b11bfb
chprat <cprater@gmx.net>
2022-04-03 10:23:35 +0200

    Move server scan function to separate file

--- catcher.js
+++ catcher.js
@@ -1,21 +1,4 @@
-function scan (ns, parent, server, list) {
-  const children = ns.scan(server)
-  for (const child of children) {
-    if (parent === child) {
-      continue
-    }
-    list.push(child)
-    scan(ns, server, child, list)
-  }
-}
-
-export function listServers (ns) {
-  const list = []
-  ns.disableLog('scan')
-  scan(ns, '', 'home', list)
-  ns.enableLog('scan')
-  return list
-}
+import { listServers } from 'imports/scanner.js'
 
 export async function files (ns, server) {
   if (!ns.hasRootAccess(server)) {
--- deployer.js
+++ deployer.js
@@ -4,6 +4,7 @@ export async function main (ns) {
   ns.disableLog('run')
   while (true) {
     ns.run('deploy.js', 1, 'weaker.js', 1)
+    ns.run('deploy.js', 1, '/imports/scanner.js', -1)
     ns.run('deploy.js', 1, 'hacker.js', 0)
     await ns.sleep(600000)
   }

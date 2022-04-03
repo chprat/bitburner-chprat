7b60d964cba2fb053a92f6ad98e150f7db57bf02
chprat <49040338+chprat@users.noreply.github.com>
2022-04-03 10:46:37 +0200

    Remove weaker.js

--- deployer.js
+++ deployer.js
@@ -3,7 +3,6 @@ export async function main (ns) {
   ns.disableLog('sleep')
   ns.disableLog('run')
   while (true) {
-    ns.run('deploy.js', 1, 'weaker.js', 1)
     ns.run('deploy.js', 1, '/imports/scanner.js', -1)
     ns.run('deploy.js', 1, 'hacker.js', 0)
     await ns.sleep(600000)
--- runner.js
+++ runner.js
@@ -2,7 +2,6 @@
 export async function main (ns) {
   ns.run('rooter.js', 1)
   ns.run('deployer.js', 1)
-  ns.run('weaker.js', 1)
   ns.run('solver.js', 1)
   ns.run('hacker.js', 80)
 }

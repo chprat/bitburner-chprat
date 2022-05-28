d93dda327fddfd745497f7eb5af4b6ece51c4565
chprat <cprater@gmx.net>
2022-05-28 10:51:50 +0200

    Add solver for ArrayJumpingGame2 contracts

---
+++ imports/arrayJumpingGame2.js
@@ -0,0 +1,17 @@
+function minJumps (data, n) {
+  if (n === 1) { return 0 }
+  let res = Number.MAX_VALUE
+  for (let i = n - 2; i >= 0; i--) {
+    if (i + data[i] >= n - 1) {
+      const subRes = minJumps(data, i + 1)
+      if (subRes !== Number.MAX_VALUE) { res = Math.min(res, subRes + 1) }
+    }
+  }
+  return res
+}
+
+export function arrayJumpingGame2 (data) {
+  if (data.length === 1) return 0
+  if (data[0] === 0) return 0
+  return minJumps(data, data.length)
+}

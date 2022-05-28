5b7d5becb4cf1f932d37ffb1d50371f3f4743eb4
chprat <cprater@gmx.net>
2022-05-28 11:36:15 +0200

    Add solver for LzCompress contracts

---
+++ imports/lzCompress.js
@@ -0,0 +1,90 @@
+export function lzCompress (data) {
+  let curState = Array.from(Array(10), () => Array(10).fill(null))
+  let newState = Array.from(Array(10), () => Array(10))
+  function set (state, i, j, str) {
+    const current = state[i][j]
+    if (current == null || str.length < current.length) {
+      state[i][j] = str
+    } else if (str.length === current.length && Math.random() < 0.5) {
+      state[i][j] = str
+    }
+  }
+
+  curState[0][1] = ''
+  for (let i = 1; i < data.length; ++i) {
+    for (const row of newState) {
+      row.fill(null)
+    }
+    const c = data[i]
+    for (let length = 1; length <= 9; ++length) {
+      const string = curState[0][length]
+      if (string == null) {
+        continue
+      }
+      if (length < 9) {
+        set(newState, 0, length + 1, string)
+      } else {
+        set(newState, 0, 1, string + '9' + data.substring(i - 9, i) + '0')
+      }
+      for (let offset = 1; offset <= Math.min(9, i); ++offset) {
+        if (data[i - offset] === c) {
+          set(newState, offset, 1, string + length + data.substring(i - length, i))
+        }
+      }
+    }
+
+    for (let offset = 1; offset <= 9; ++offset) {
+      for (let length = 1; length <= 9; ++length) {
+        const string = curState[offset][length]
+        if (string == null) {
+          continue
+        }
+        if (data[i - offset] === c) {
+          if (length < 9) {
+            set(newState, offset, length + 1, string)
+          } else {
+            set(newState, offset, 1, string + '9' + offset + '0')
+          }
+        }
+
+        set(newState, 0, 1, string + length + offset)
+        for (let newOffset = 1; newOffset <= Math.min(9, i); ++newOffset) {
+          if (data[i - newOffset] === c) {
+            set(newState, newOffset, 1, string + length + offset + '0')
+          }
+        }
+      }
+    }
+    const tmpState = newState
+    newState = curState
+    curState = tmpState
+  }
+  let result = null
+  for (let len = 1; len <= 9; ++len) {
+    let string = curState[0][len]
+    if (string == null) {
+      continue
+    }
+    string += len + data.substring(data.length - len, data.length)
+    if (result == null || string.length < result.length) {
+      result = string
+    } else if (string.length === result.length && Math.random() < 0.5) {
+      result = string
+    }
+  }
+  for (let offset = 1; offset <= 9; ++offset) {
+    for (let len = 1; len <= 9; ++len) {
+      let string = curState[offset][len]
+      if (string == null) {
+        continue
+      }
+      string += len + '' + offset
+      if (result == null || string.length < result.length) {
+        result = string
+      } else if (string.length === result.length && Math.random() < 0.5) {
+        result = string
+      }
+    }
+  }
+  return result !== null && result !== undefined ? result : ''
+}

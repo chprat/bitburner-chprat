a1dac8f514e96a8643d8ed3d9c3036e3dc6ed592
chprat <49040338+chprat@users.noreply.github.com>
2022-06-11 16:13:53 +0200

    Initialize linter if not present

--- lint.sh
+++ lint.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
+if [ ! -d node_modules ]; then
+    docker run -it --rm -v $(pwd):/data --entrypoint="" cytopia/eslint /bin/sh -c "apk add npm && npm install"
+fi
+
 args=""
+
 while getopts f flag
 do
     case "${flag}" in
         f) args="--fix ";;
     esac
 done
+
 docker run -it --rm -v $(pwd):/data cytopia/eslint . $args
+

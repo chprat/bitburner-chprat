ed23813f964de33056544323c2d4375c886e6f20
chprat <49040338+chprat@users.noreply.github.com>
2022-06-25 10:33:58 +0200

    Add log output to hacknet script

--- hacknet.js
+++ hacknet.js
@@ -7,13 +7,36 @@ export async function upgrade (ns, nodeId) {
   upgrades.find(e => e.type === 'core').cost = ns.hacknet.getCoreUpgradeCost(nodeId, 1)
   upgrades.find(e => e.type === 'level').cost = ns.hacknet.getLevelUpgradeCost(nodeId, 1)
   const availUpgrades = upgrades.filter(e => e.cost !== Infinity).sort(function (a, b) { return a.cost - b.cost })
-  if (availUpgrades.length === 0) { return false }
-  if (availUpgrades[0].cost > ns.getPlayer().money) { return false }
-  if (availUpgrades[0].cost > ns.hacknet.getPurchaseNodeCost()) {
-    if ((ns.hacknet.getPurchaseNodeCost() < newNodeMaxCost) && (ns.getPlayer().money > newNodeMaxCost)) { ns.hacknet.purchaseNode() }
+  if (availUpgrades.length === 0) {
+    ns.print(`No upgrade available for node ${nodeId}`)
     return false
   }
-  if (availUpgrades[0].type === 'ram') { ns.hacknet.upgradeRam(nodeId, 1) } else if (availUpgrades[0].type === 'core') { ns.hacknet.upgradeCore(nodeId, 1) } else if (availUpgrades[0].type === 'level') { ns.hacknet.upgradeLevel(nodeId, 1) } else { return false }
+  if (availUpgrades[0].cost > ns.getPlayer().money) {
+    ns.print(`Upgrades to expensive for ${nodeId}`)
+    return false
+  }
+  const newNodeCost = ns.hacknet.getPurchaseNodeCost()
+  if (availUpgrades[0].cost > newNodeCost) {
+    ns.print(`Upgrades to expensive for ${nodeId}, prefer buying a new node instead`)
+    if ((newNodeCost < newNodeMaxCost) && (ns.getPlayer().money > newNodeCost)) {
+      ns.print(`Upgrades to expensive for ${nodeId}, buying a new node instead`)
+      ns.hacknet.purchaseNode()
+    }
+    return false
+  }
+  if (availUpgrades[0].type === 'ram') {
+    ns.print(`Upgrading RAM for node ${nodeId}`)
+    ns.hacknet.upgradeRam(nodeId, 1)
+  } else if (availUpgrades[0].type === 'core') {
+    ns.print(`Upgrading CPU for node ${nodeId}`)
+    ns.hacknet.upgradeCore(nodeId, 1)
+  } else if (availUpgrades[0].type === 'level') {
+    ns.print(`Upgrading level for node ${nodeId}`)
+    ns.hacknet.upgradeLevel(nodeId, 1)
+  } else {
+    ns.print(`Unknown update type for node ${nodeId}`)
+    return false
+  }
   return true
 }
 

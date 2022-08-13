7827284392a725dd21d38f988d16f7de95693c72
chprat <49040338+chprat@users.noreply.github.com>
2022-08-13 11:48:32 +0200

    Add script to upgrade home PC

--- deployer.js
+++ deployer.js
@@ -44,10 +44,11 @@ export async function onHome (ns) {
     { name: 'corp.js', threads: 0, mem: 0 },
     { name: 'gang.js', threads: 1, mem: 0 },
     { name: 'hacknet.js', threads: 1, mem: 0 },
-    { name: 'psrv.js', threads: 1, mem: 0 }
+    { name: 'psrv.js', threads: 1, mem: 0 },
+    { name: 'upgrader.js', threads: 1, mem: 0 }
   ]
   const optionalScripts = ['solver.js', 'trader.js', 'corp.js']
-  const waitForScripts = ['gang.js', 'hacknet.js', 'psrv.js']
+  const waitForScripts = ['gang.js', 'hacknet.js', 'psrv.js', 'upgrader.js']
   for (const script of scripts) {
     script.mem = ns.getScriptRam(script.name)
   }
---
+++ upgrader.js
@@ -0,0 +1,29 @@
+function upgradePC (ns) {
+  const canUpgradeRAM = ns.singularity.getUpgradeHomeRamCost() <= ns.getPlayer().money
+  if (canUpgradeRAM) {
+    const success = ns.singularity.upgradeHomeRam()
+    if (!success) {
+      ns.print("Couldn't upgrade home RAM")
+    } else {
+      ns.print('Upgraded home RAM')
+    }
+  } else {
+    ns.print('Not enough money to upgrade home RAM')
+  }
+  const canUpgradeCores = ns.singularity.getUpgradeHomeCoresCost() <= ns.getPlayer().money
+  if (canUpgradeCores) {
+    const success = ns.singularity.upgradeHomeCores()
+    if (!success) {
+      ns.print("Couldn't upgrade home cores")
+    } else {
+      ns.print('Upgraded home cores')
+    }
+  } else {
+    ns.print('Not enough money to upgrade home cores')
+  }
+}
+
+/** @param {NS} ns **/
+export async function main (ns) {
+  upgradePC(ns)
+}

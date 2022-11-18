38d0ec109101edd8314d776182604a49112c20eb
chprat <49040338+chprat@users.noreply.github.com>
2022-11-18 17:11:14 +0100

    worker: Split off getPrograms() to workHelpers.js

---
+++ imports/workHelpers.js
@@ -0,0 +1,16 @@
+export function getPrograms (ns) {
+  ns.print('Check if we need a program')
+  const programs = [
+    { name: 'BruteSSH.exe', exists: false },
+    { name: 'FTPCrack.exe', exists: false },
+    { name: 'relaySMTP.exe', exists: false },
+    { name: 'HTTPWorm.exe', exists: false },
+    { name: 'SQLInject.exe', exists: false }
+  ]
+  programs.find(e => e.name === 'BruteSSH.exe').exists = ns.fileExists('BruteSSH.exe', 'home')
+  programs.find(e => e.name === 'FTPCrack.exe').exists = ns.fileExists('FTPCrack.exe', 'home')
+  programs.find(e => e.name === 'relaySMTP.exe').exists = ns.fileExists('relaySMTP.exe', 'home')
+  programs.find(e => e.name === 'HTTPWorm.exe').exists = ns.fileExists('HTTPWorm.exe', 'home')
+  programs.find(e => e.name === 'SQLInject.exe').exists = ns.fileExists('SQLInject.exe', 'home')
+  return programs
+}
--- worker.js
+++ worker.js
@@ -1,5 +1,6 @@
 import { getFactionsSortedByMissingRep, getCompanies, joinedFaction } from 'imports/factionHelpers.js'
 import { hasMissingAugs } from 'imports/augmentationHelpers.js'
+import { getPrograms } from 'imports/workHelpers.js'
 
 let focus = true
 
@@ -93,18 +94,7 @@ function doCompanyWork (ns) {
 
 function buyOrCreateProgram (ns) {
   ns.print('Check if we need a program')
-  const programs = [
-    { name: 'BruteSSH.exe', exists: false },
-    { name: 'FTPCrack.exe', exists: false },
-    { name: 'relaySMTP.exe', exists: false },
-    { name: 'HTTPWorm.exe', exists: false },
-    { name: 'SQLInject.exe', exists: false }
-  ]
-  programs.find(e => e.name === 'BruteSSH.exe').exists = ns.fileExists('BruteSSH.exe', 'home')
-  programs.find(e => e.name === 'FTPCrack.exe').exists = ns.fileExists('FTPCrack.exe', 'home')
-  programs.find(e => e.name === 'relaySMTP.exe').exists = ns.fileExists('relaySMTP.exe', 'home')
-  programs.find(e => e.name === 'HTTPWorm.exe').exists = ns.fileExists('HTTPWorm.exe', 'home')
-  programs.find(e => e.name === 'SQLInject.exe').exists = ns.fileExists('SQLInject.exe', 'home')
+  const programs = getPrograms(ns)
   const hasTor = ns.singularity.purchaseTor()
   for (const program of programs) {
     if (program.exists) {
